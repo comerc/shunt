745223ec46565902e19a7b42ea2235e84651dd72
comerc <andrew.kachanov@gmail.com>
2021-08-15 15:43:34 +0300

    Change Config.Forward to map

--- config/structs.go
+++ config/structs.go
@@ -4,11 +4,6 @@ const filename = ".config.yml"
 
 type Config struct {
 	Main     int64
-	Cancel   int64
-	Forwards []Forward
-}
-
-type Forward struct {
-	From   int64
-	CopyTo int64
+	Trash    int64
+	Forwards map[int64]int64
 }
